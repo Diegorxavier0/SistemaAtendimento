4dfeb85b54394e0ff4ee5468841bcee954807a53
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-02 22:16:30 -0300

    criando formulario cadastro de etapas

--- View/FrmCadastroEtapa.Designer.cs
+++ View/FrmCadastroEtapa.Designer.cs
@@ -29,16 +29,68 @@
         private void InitializeComponent()
         {
             grbDadosEtapa = new GroupBox();
+            lblCodigo = new Label();
+            txtCodigo = new TextBox();
+            lblNome = new Label();
+            label3 = new Label();
+            txtNome = new TextBox();
+            grbDadosEtapa.SuspendLayout();
             SuspendLayout();
             // 
             // grbDadosEtapa
             // 
+            grbDadosEtapa.Controls.Add(txtNome);
+            grbDadosEtapa.Controls.Add(label3);
+            grbDadosEtapa.Controls.Add(lblNome);
+            grbDadosEtapa.Controls.Add(txtCodigo);
+            grbDadosEtapa.Controls.Add(lblCodigo);
             grbDadosEtapa.Location = new Point(16, 8);
             grbDadosEtapa.Name = "grbDadosEtapa";
             grbDadosEtapa.Size = new Size(776, 148);
             grbDadosEtapa.TabIndex = 0;
             grbDadosEtapa.TabStop = false;
             grbDadosEtapa.Text = "Dados da Etapa";
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Location = new Point(16, 24);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(46, 15);
+            lblCodigo.TabIndex = 0;
+            lblCodigo.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(16, 40);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(112, 23);
+            txtCodigo.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(144, 24);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 2;
+            lblNome.Text = "Nome";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 3;
+            label3.Text = "label3";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(144, 40);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(616, 23);
+            txtNome.TabIndex = 4;
             // 
             // FrmCadastroEtapa
             // 
@@ -48,11 +100,18 @@
             Controls.Add(grbDadosEtapa);
             Name = "FrmCadastroEtapa";
             Text = "Cadastro de Etapas";
+            grbDadosEtapa.ResumeLayout(false);
+            grbDadosEtapa.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox grbDadosEtapa;
+        private TextBox txtCodigo;
+        private Label lblCodigo;
+        private Label label3;
+        private Label lblNome;
+        private TextBox txtNome;
     }
 }
