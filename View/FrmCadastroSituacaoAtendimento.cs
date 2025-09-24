852e0a41354c1d8837f21f4050f61f38f06b7e00
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-23 22:19:41 -0300

    Alimentando o formulario situação Atendimentos

--- View/FrmCadastroSituacaoAtendimento.cs
+++ View/FrmCadastroSituacaoAtendimento.cs
@@ -27,5 +27,49 @@ namespace SistemaAtendimento.View
         {
             dgvListaSituacaoAtendimento.DataSource = situacaoAtendimentos;
         }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+           SituacaoAtendimentos situacaoAtendimentos = new SituacaoAtendimentos
+            {
+                Nome = txtNome.Text,
+                Cor = txtCor.Text,
+                Ativo = rdbAtivo.Checked,
+
+            };
+
+            if (!ValidarDados(cliente))
+                return;
+
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text))
+            {
+                //novo cliente
+                _clienteController.Salvar(cliente);
+            }
+            else
+            {
+                //editar cliente
+                cliente.Id = Convert.ToInt32(txtCodigo.Text);
+                //implementar metodo editar no controller e repository
+                _clienteController.Atualizar(cliente);
+            }
+        }
+        public bool ValidarDados(SituacaoAtendimentos situacaoAtendimentos)
+        {
+            if (string.IsNullOrWhiteSpace(txtNome.Text))
+            {
+                ExibirMensagem("O campo Nome é obrigatório.");
+                txtNome.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCor.Text))
+            {
+                ExibirMensagem("O campo Cor é obrigatório.");
+                txtCor.Focus();
+                return false;
+            }
+
+            //terminar proxima aula
+        }
     }
-}
